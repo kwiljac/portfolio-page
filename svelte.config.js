c9a0cfe1fe891a99504db21b1dadc8e65de996d8
kwiljac <109763824+kwiljac@users.noreply.github.com>
2024-08-31 21:25:09 -0400

    2nd commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter(),
+    }
 };
+
 export default config;
